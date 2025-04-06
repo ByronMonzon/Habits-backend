3eb00319de7f169007a5d880088f3a97540f3840
ByronM <monzonbyron43@gmail.com>
2025-04-06 17:26:42 -0600

    Agregado CORS al backend

--- app.js
+++ app.js
@@ -12,13 +12,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://habits-frontend-henna.vercel.app',
   credentials: true
 }));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -42,7 +38,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({'error':err});
 });
 
 module.exports = app;
